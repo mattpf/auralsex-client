4b6595829cd2649872a9d2fbc28bcfd5d4f756b1
Matthew Powers-Freeling <mattpf@mit.edu>
2010-10-09 11:10:45 -0400

    Hostnames > IPs

--- zones.py
+++ zones.py
@@ -2,5 +2,5 @@ zones = {
     'local': '127.0.0.1:8000',
     'bemis': 'auralsex.mit.edu:8000',
     'walcott': 'auralsex2.mit.edu:8000',
-    'drum': '18.238.6.13:8000',
+    'drum': 'eastcampus-seven-eighty.mit.edu:8000',
 }
