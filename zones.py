6dd2910821364bdc037169e76440641fa548ce2e
Matthew Powers-Freeling <mattpf@mit.edu>
2010-10-10 11:16:22 -0400

    Meddled with server hostnames.

--- zones.py
+++ zones.py
@@ -1,6 +1,5 @@
 zones = {
     'local': '127.0.0.1:8000',
     'bemis': 'auralsex.mit.edu:8000',
-    'walcott': 'auralsex2.mit.edu:8000',
-    'drum': 'eastcampus-seven-eighty.mit.edu:8000',
+    'drum': 'auralsex2.mit.edu:8000',
 }
