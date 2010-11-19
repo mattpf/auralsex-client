6c177d9b2fb26a5f5ab493fd33c5037a9715683d
Matthew Powers-Freeling <mattpf@mit.edu>
2010-11-18 19:31:19 -0500

    Added logging of changes to the music
    - Writes to action_log.txt in its directory
    - Doesn't log volume changes.

--- api.py
+++ api.py
@@ -2,11 +2,23 @@ import cherrypy
 import sqlite3
 import json
 import urllib2, urllib
+import time
+from threading import Lock
 
 import config
+import main
 
 class AuralAPI(object):
     """The exposed API for the UI"""
+    log_lock = Lock()
+    
+    def current_user(self, ):
+        return cherrypy.request.wsgi_environ['REMOTE_USER'] if 'REMOTE_USER' in cherrypy.request.wsgi_environ else None
+    
+    def log_event(self, zone, event):
+        with self.log_lock:
+            with open('%s/action_log.txt' % main.current_dir, 'a+') as log:
+                log.write("[%s] [%s] [%s] %s\n" % (time.strftime('%x %X'), zone, self.current_user(), event))
     
     @cherrypy.expose
     def library(self, **args):
@@ -63,23 +75,28 @@ class AuralAPI(object):
     
     @cherrypy.expose
     def pause(self, zone):
+        self.log_event(zone, "toggled pause")
         return "{success: %s}" % self.command(zone, "pause")
     
     @cherrypy.expose
     def play(self, zone, track_id=None, **args):
         if track_id is None:
             raise cherry.HTTPError(410)
+        self.log_event(zone, "played track #%s" % track_id)
         self.command(zone, "play", {'filename': self.id_to_filename(track_id)})
         
     @cherrypy.expose
     def skip(self, zone, to=None):
         if to is None:
+            self.log_event(zone, "skipped to next track")
             return "{success: %s}" % self.command(zone, "skip")
         else:
+            self.log_event(zone, "skipped to track #%s" % to)
             return "{success: %s}" % self.command(zone, "skip", {'to': to})
         
     @cherrypy.expose
     def back(self, zone):
+        self.log_event(zone, "skipped back one track")
         return "{success: %s}" % self.command(zone, "back")
     
     @cherrypy.expose
@@ -96,6 +113,7 @@ class AuralAPI(object):
     def append(self, zone, track_id=None, **args):
         if track_id is None:
             raise cherry.HTTPError(410)
+        self.log_event(zone, "added track #%s to the queue" % track_id)
         return "{success: %s}" % self.command(zone, "add", {'filename': self.id_to_filename(track_id)})
     
     @cherrypy.expose
@@ -122,6 +140,7 @@ class AuralAPI(object):
     
     @cherrypy.expose
     def remove(self, zone, index, **args):
+        self.log_event(zone, "removed track(s) #%s from the queue" % index)
         return "{success: %s}" % self.command(zone, "remove", {'index': index})
     
     @cherrypy.expose
@@ -136,7 +155,7 @@ class AuralAPI(object):
         try:
             track_id, title, artist, album = c.fetchone()
         except:
-            track_id, title, artist, album = None, filename, None, None
+            track_id, title, artist, album = None, current_file, None, None
         c.close()
         db.close()
         return json.dumps({'playing': {'track_id': track_id, 'title': title, 'artist': artist, 'album': album}})
